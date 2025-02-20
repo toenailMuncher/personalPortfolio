dbc529654e28dd4502d36440cfbe03d49be7f9c6
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-02-20 15:05:57 -0500

    dark mode.

--- script.js
+++ script.js
@@ -4,19 +4,50 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-
-
-// filepath: /C:/Users/marcu/Documents/GitHub/personalPortfolio/script.js
-const themeToggle = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme') || 'light';
-
-if (currentTheme === 'dark') {
-  document.documentElement.setAttribute('data-theme', 'dark');
-}
-
-themeToggle.addEventListener('click', () => {
-  const theme = document.documentElement.getAttribute('data-theme');
-  const newTheme = theme === 'dark' ? 'light' : 'dark';
-  document.documentElement.setAttribute('data-theme', newTheme);
-  localStorage.setItem('theme', newTheme);
-});
+  
+  // Dark / light mode
+  
+  const btn = document.getElementById("modeToggle");
+  const btn2 = document.getElementById("modeToggle2");
+  const themeIcons = document.querySelectorAll(".icon");
+  const currentTheme = localStorage.getItem("theme");
+  
+  if (currentTheme === "dark") {
+    setDarkMode();
+  }
+  
+  btn.addEventListener("click", function () {
+    setTheme();
+  });
+  
+  btn2.addEventListener("click", function () {
+    setTheme();
+  });
+  
+  function setTheme() {
+    let currentTheme = document.body.getAttribute("theme");
+  
+    if (currentTheme === "dark") {
+      setLightMode();
+    } else {
+      setDarkMode();
+    }
+  }
+  
+  function setDarkMode() {
+    document.body.setAttribute("theme", "dark");
+    localStorage.setItem("theme", "dark");
+  
+    themeIcons.forEach((icon) => {
+      icon.src = icon.getAttribute("src-dark");
+    });
+  }
+  
+  function setLightMode() {
+    document.body.removeAttribute("theme");
+    localStorage.setItem("theme", "light");
+  
+    themeIcons.forEach((icon) => {
+      icon.src = icon.getAttribute("src-light");
+    });
+  }
